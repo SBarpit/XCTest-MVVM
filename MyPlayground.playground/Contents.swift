1d059fa3ef31c9f46ec002e12b5574e3a62c1f96
Arpit <arpit.srivastav@appinventiv.com>
2021-08-28 12:19:26 +0530

    Removed boilerplate code.

--- MyPlayground.playground/Contents.swift
+++
@@ -1,8 +0,0 @@
-
-let c = "dvsdnvklfnkvf ,md fvmfn kjv"
-let a = c.split(separator: " ")
-
-let str = "1 2 3 4 5 6"
-let array:[String] = str.split(separator: " ")
-let ar = array.joined(separator: " ")
-print(array)
